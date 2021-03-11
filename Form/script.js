d9aa06254a94991376a91208933f9ae142d68571
konoam <40669401+konoam@users.noreply.github.com>
2021-03-11 09:36:27 +0200

    from local to git all files

---
+++ Form/script.js
@@ -0,0 +1,9 @@
+function gradeChange(){
+    var value = document.getElementById("grade").value;
+    document.getElementById('curr-grade').innerHTML=value
+
+}
+
+function submitForm(){
+
+}
---
+++ Menu/Vertical/script.js
@@ -0,0 +1,18 @@
+function showAbout(){
+    document.getElementById('about').style.display='block'
+    document.getElementById('products').style.display='none'
+    document.getElementById('services').style.display='none'                    
+}
+
+
+function showProducts(){
+    document.getElementById('about').style.display='none'
+    document.getElementById('products').style.display='block'
+    document.getElementById('services').style.display='none'                    
+}
+
+function showServices(){
+    document.getElementById('about').style.display='none'
+    document.getElementById('products').style.display='none'
+    document.getElementById('services').style.display='block'                    
+}
