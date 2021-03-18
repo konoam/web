a9b2df82b1445d38657577323da8e36110768c07
konoam <40669401+konoam@users.noreply.github.com>
2021-03-18 14:14:07 +0200

    end of day

---
+++ js/code/script.js
@@ -0,0 +1,13 @@
+
+let x = '3';
+console.log('value of `x` is: ' +x)
+console.log(typeof(x))
+x='noam'
+console.log('value of `x` is: ' +x)
+console.log(typeof(x))
+
+
+
+
+
+
---
+++ js/objects/script.js
@@ -0,0 +1,25 @@
+
+let noam = {
+    eyes:"brown",
+    hair:"brown",
+    name:"noam",    
+} ;
+
+console.log(noam)
+
+noam.eyes="green"
+console.log(noam)
+noam.location = {israel:true,
+                 city:"tlv"}
+console.log(noam.location)
+noam.lang ={ english: true, hebrew:true, russion:false}
+console.log(noam.lang)
+
+
+
+
+
+
+
+
+
